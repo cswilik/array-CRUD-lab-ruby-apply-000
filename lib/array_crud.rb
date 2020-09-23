7e1e22bc092a7c73afe4ff6c67af99cc565f3456
Chelsey <cswilik@gmail.com>
2020-09-23 17:06:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["cats","wow"]
+  array.shift(0)
 end
 
 def retrieve_element_from_index(array, index_number)
